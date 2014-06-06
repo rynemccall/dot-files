22a014188facb5366b5471d049f50933ef3b0643
Ryne McCall <ryne@rynemccall.com>
2014-06-05 23:09:13 -0400

    Add emacs configuration

---
+++ home/.emacs.d/init.el
@@ -0,0 +1,14 @@
+;; Save backup files in temporary directory
+;; http://www.emacswiki.org/emacs/BackupDirectory#toc2
+(setq backup-directory-alist
+  `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+  `((".*" ,temporary-file-directory t)))
+
+(require 'package)
+(add-to-list 'package-archives
+  '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(package-initialize)
+
+(global-nlinum-mode)
